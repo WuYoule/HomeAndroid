b41a47c9e6a59af24cacd94e1578c2b07b48194b
WuYoule <2367482229@qq.com>
2016-01-22 18:03:23 +0800

    地图相关

--- Dianping/src/com/wqy/dianping/NearbyMapActivity.java
+++ Dianping/src/com/wqy/dianping/NearbyMapActivity.java
@@ -1,61 +1,58 @@
 package com.wqy.dianping;
 
-import java.sql.Date;
-import java.text.SimpleDateFormat;
-
-import com.amap.api.location.AMapLocation;
-import com.amap.api.location.AMapLocationClient;
-import com.amap.api.location.AMapLocationClientOption;
-import com.amap.api.location.AMapLocationListener;
-import com.amap.api.location.AMapLocationClientOption.AMapLocationMode;
-import com.amap.api.mapcore.util.am;
-import com.amap.api.maps.AMap;
-import com.amap.api.maps.LocationSource;
-import com.amap.api.maps.MapView;
 
 
-import com.lidroid.xutils.ViewUtils;
-import com.lidroid.xutils.view.annotation.ViewInject;
+
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.TextView;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationClientOption.AMapLocationMode;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.mapcore.t;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.MapView;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
 
 
-//地图
-public class NearbyMapActivity extends Activity implements LocationSource,AMapLocationListener{
-	
-    @ViewInject(R.id.search_mymap)
-	private MapView mapView;
+/**
+ * AMapV2地图中介绍定位三种模式的使用，包括定位，追随，旋转
+ */
+public class NearbyMapActivity extends Activity implements LocationSource,
+		AMapLocationListener {
 	private AMap aMap;
-	
+	@ViewInject(R.id.search_mymap)
+	private MapView mapView;
 	private OnLocationChangedListener mListener;
 	private AMapLocationClient mlocationClient;
 	private AMapLocationClientOption mLocationOption;
+	private RadioGroup mGPSModeGroup;
 	
-//	private double lon;
-//	private double lan;
-	
-	
+	private TextView mLocationErrText;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.search_index);
+//		requestWindowFeature(Window.FEATURE_NO_TITLE);// 不显示程序的标题栏
+		setContentView(R.layout.search_map);
+	
 		ViewUtils.inject(this);
-		
-		mapView.onCreate(savedInstanceState);
+		mapView.onCreate(savedInstanceState);// 此方法必须重写
 		init();
-//		if (aMap==null) {
-//			aMap=mapView.getMap();
-//			aMap.setLocationSource(this);//设置了定位监听
-//			aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
-//			aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
-//			// 设置定位的类型为定位模式，参见类AMap。
-//			aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
-//		}
 	}
+
 	/**
 	 * 初始化
 	 */
@@ -64,8 +61,12 @@ public class NearbyMapActivity extends Activity implements LocationSource,AMapLo
 			aMap = mapView.getMap();
 			setUpMap();
 		}
-		
+//		mGPSModeGroup = (RadioGroup) findViewById(R.id.gps_radio_group);
+//		mGPSModeGroup.setOnCheckedChangeListener(this);
+////		mLocationErrText = (TextView)findViewById(R.id.location_errInfo_text);
+//		mLocationErrText.setVisibility(View.GONE);
 	}
+
 	/**
 	 * 设置一些amap的属性
 	 */
@@ -77,37 +78,87 @@ public class NearbyMapActivity extends Activity implements LocationSource,AMapLo
 		aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
 	}
 
+//	@Override
+//	public void onCheckedChanged(RadioGroup group, int checkedId) {
+//		switch (checkedId) {
+//		case R.id.gps_locate_button:
+//			// 设置定位的类型为定位模式
+//			aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
+//			break;
+//		case R.id.gps_follow_button:
+//			// 设置定位的类型为 跟随模式
+//			aMap.setMyLocationType(AMap.LOCATION_TYPE_MAP_FOLLOW);
+//			break;
+//		case R.id.gps_rotate_button:
+//			// 设置定位的类型为根据地图面向方向旋转
+//			aMap.setMyLocationType(AMap.LOCATION_TYPE_MAP_ROTATE);
+//			break;
+//		}
+//
+//	}
+
+	/**
+	 * 方法必须重写
+	 */
 	@Override
 	protected void onResume() {
-		// TODO Auto-generated method stub
 		super.onResume();
 		mapView.onResume();
 	}
+
+	/**
+	 * 方法必须重写
+	 */
 	@Override
 	protected void onPause() {
-		// TODO Auto-generated method stub
 		super.onPause();
 		mapView.onPause();
 		deactivate();
 	}
+
+	/**
+	 * 方法必须重写
+	 */
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		mapView.onSaveInstanceState(outState);
+	}
+
+	/**
+	 * 方法必须重写
+	 */
 	@Override
 	protected void onDestroy() {
-		// TODO Auto-generated method stub
 		super.onDestroy();
 		mapView.onDestroy();
 		if(null != mlocationClient){
 			mlocationClient.onDestroy();
 		}
 	}
+
+	/**
+	 * 定位成功后回调函数
+	 */
 	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		// TODO Auto-generated method stub
-		super.onSaveInstanceState(outState);
-		mapView.onSaveInstanceState(outState);
+	public void onLocationChanged(AMapLocation amapLocation) {
+		if (mListener != null && amapLocation != null) {
+			if (amapLocation != null
+					&& amapLocation.getErrorCode() == 0) {
+//				mLocationErrText.setVisibility(View.GONE);
+				mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
+			} else {
+				String errText = "定位失败," + amapLocation.getErrorCode()+ ": " + amapLocation.getErrorInfo();
+				Log.e("AmapErr",errText);
+//				mLocationErrText.setVisibility(View.VISIBLE);
+//				mLocationErrText.setText(errText);
+			}
+		}
 	}
 
-
-
+	/**
+	 * 激活定位
+	 */
 	@Override
 	public void activate(OnLocationChangedListener listener) {
 		mListener = listener;
@@ -126,9 +177,11 @@ public class NearbyMapActivity extends Activity implements LocationSource,AMapLo
 			// 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
 			mlocationClient.startLocation();
 		}
-		
 	}
 
+	/**
+	 * 停止定位
+	 */
 	@Override
 	public void deactivate() {
 		mListener = null;
@@ -137,23 +190,8 @@ public class NearbyMapActivity extends Activity implements LocationSource,AMapLo
 			mlocationClient.onDestroy();
 		}
 		mlocationClient = null;
-		
 	}
 
-	@Override
-	public void onLocationChanged(AMapLocation amapLocation) {
-		if (mListener != null && amapLocation != null) {
-			if (amapLocation != null
-					&& amapLocation.getErrorCode() == 0) {
-//				mLocationErrText.setVisibility(View.GONE);
-				mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
-			} else {
-				String errText = "定位失败," + amapLocation.getErrorCode()+ ": " + amapLocation.getErrorInfo();
-				Log.i("AmapErr",errText);
-//				mLocationErrText.setVisibility(View.VISIBLE);
-//				mLocationErrText.setText(errText);
-			}
-		}
-		
-	}
+	
 }
+
--- Dianping/src/com/wqy/dianping/fragment/FragmentSearch.java
+++ Dianping/src/com/wqy/dianping/fragment/FragmentSearch.java
@@ -1,8 +1,12 @@
 package com.wqy.dianping.fragment;
 
 
+import java.net.NetPermission;
+
+import com.wqy.dianping.NearbyMapActivity;
 import com.wqy.dianping.R;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -16,6 +20,7 @@ public class FragmentSearch extends Fragment {
 			Bundle savedInstanceState) {
 		
 		View view=inflater.inflate(R.layout.search_index, null);
+		startActivity(new Intent(getActivity(), NearbyMapActivity.class));
 		return view;
 		
 	}
