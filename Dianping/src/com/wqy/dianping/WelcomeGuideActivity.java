51cb9ec8a69b7d82685232241e4c9b4e61347f07
Wqimai <qiyan.wu@maimob.cn>
2017-01-18 18:41:10 +0800

    修改 挪到androidstudio中

--- Dianping/src/com/wqy/dianping/WelcomeGuideActivity.java
+++ Dianping/src/com/wqy/dianping/WelcomeGuideActivity.java
@@ -8,6 +8,7 @@ import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.lidroid.xutils.view.annotation.event.OnClick;
 
+import android.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
